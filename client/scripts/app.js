fbe9462ef331975522b2df1511c23cd2545dd329
charliegphan <charliegphan@gmail.com>
2018-08-11 16:36:10 -0700

    complete room rendering

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,7 +2,8 @@
 var app = {
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
   friends: [],
-  rooms: []
+  rooms: [],
+  data: undefined,
 };
 
 app.init = function() {
@@ -25,11 +26,14 @@ app.init = function() {
     var valueSelected  = optionSelected.val();
 
     console.log(valueSelected);
+    app.renderRoom(valueSelected);
   });  
   
   app.fetch(
     'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
   );
+
+
 };
 
 app.send = function(obj) {
@@ -52,10 +56,25 @@ app.fetch = function(url) {
   return $.ajax(url, {
     type: 'GET',
     contentType: 'application/json',
-    data: 'order=-createdAt',
+    // data: 'order=-createdAt',
     success: function(data) {
       console.log(data);
-      var acceptableMessages = data.results //.filter (function(obj) {
+      // debugger;
+      app.data = data.results;
+      
+      for (var i = 0; i < data.results.length; i++) {
+        app.renderMessage(data.results[i]);
+      }
+
+      app.addRoom(data.results);
+      
+
+
+    },
+    error: function(data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+    //.filter (function(obj) {
         // if (obj.username !== undefined && 
         //   obj.text !== undefined &&
         //   obj.text.length > 280 &&
@@ -67,16 +86,6 @@ app.fetch = function(url) {
         // }        
       // });
 
-      console.log(acceptableMessages);
-
-      for (var i = 0; i < acceptableMessages.length; i++) {
-        app.renderMessage(acceptableMessages[i]);
-      }
-      app.addRoom(acceptableMessages)
-    },
-    error: function(data) {
-      console.error('chatterbox: Failed to send message', data);
-    }
   });
 };
 
@@ -85,20 +94,28 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(obj) {
-  // var username = app.sanitize(obj.username);
-  // if (obj.text === undefined) {
-  //   var text = "";
-  // } else {
-  //   var text = app.sanitize(obj.text);
-  // }
-
-  // var rooomname = app.sanitize(obj.roomname);
-
-  $('#chats').append('<div class="username">' + obj.username + '-'+ obj.text + '</div>');
+  var newNode = $('<div></div>');
+  newNode.text(obj.username);
+  $('#chats').append(newNode);
 };
 
-app.renderRoom = function(string) {
-  $('#roomSelect').append('<p>sorry</p>');
+app.renderRoom = function(nameOfRoom) {
+  app.clearMessages();
+
+  var filteredObjects = app.data.filter((obj) => {
+    return obj.roomname === nameOfRoom;
+  })
+
+  filteredObjects.forEach((obj) => {
+    app.renderMessage(obj);
+  })
+
+  // for (var i = 0; i < app.data.length; i++) {
+  //   if (app.data[i].roomname === nameOfRoom) {
+  //     app.
+  //   }
+  // }
+
 };
 
 app.handleUsernameClick = function(username) {
@@ -119,11 +136,23 @@ app.addRoom = function(results) {
   for (var i = 0; i < results.length; i++) {
     rooms[results[i].roomname] = true;
   }
+  console.log(rooms);
 
   for (var i = 0; i < Object.keys(rooms).length; i++) {
-    $(".rooms").append('<option value=' + Object.keys(rooms)[i] + '>'+ Object.keys(rooms)[i] + '</option>');
-    // $(".rooms").append('<option>'+ Object.keys(rooms)[i] + '</option>');
+    var newNode = $('<option></option>')
+    newNode.text(Object.keys(rooms)[i]);
+    // $(".rooms").append('<option value=' + Object.keys(rooms)[i] + '>'+ Object.keys(rooms)[i] + '</option>');
+    $(".rooms").append(newNode);
   }
 }
 
 app.init();
+
+  // var username = app.sanitize(obj.username);
+  // if (obj.text === undefined) {
+  //   var text = "";
+  // } else {
+  //   var text = app.sanitize(obj.text);
+  // }
+
+  // var rooomname = app.sanitize(obj.roomname);
